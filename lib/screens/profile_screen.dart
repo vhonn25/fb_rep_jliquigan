1e14f0c80edb659c8f48a97318f410ae379bf17b
jvliquigan25 <jvliquigan25@gmail.com>
2025-02-21 18:10:24 +0000

    Updated code

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -11,7 +11,11 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 class ProfileScreen extends StatefulWidget {
   final String username;
   final String profileImageUrl;
-  const ProfileScreen({super.key, required this.username, required this.profileImageUrl,});
+  const ProfileScreen({
+    super.key,
+    required this.username,
+    required this.profileImageUrl,
+  });
 
   @override
   State<ProfileScreen> createState() => _ProfileScreenState();
@@ -25,7 +29,8 @@ class _ProfileScreenState extends State<ProfileScreen> {
 
   @override
   Widget build(BuildContext context) {
-    const String profileImageUrl = 'https://gdm-assets.b-cdn.net/images/ncavvykf/epicstream/c1e0142f06accc89f5ccdba411b09401a95f0cde-1280x720.jpg?rect=0,26,1280,669&w=398&h=208&auto=format';
+    const String profileImageUrl =
+        'https://gdm-assets.b-cdn.net/images/ncavvykf/epicstream/c1e0142f06accc89f5ccdba411b09401a95f0cde-1280x720.jpg?rect=0,26,1280,669&w=398&h=208&auto=format';
     return DefaultTabController(
       length: 3,
       child: Container(
@@ -42,7 +47,8 @@ class _ProfileScreenState extends State<ProfileScreen> {
                     decoration: BoxDecoration(
                       color: Colors.grey[300],
                       image: const DecorationImage(
-                        image: CachedNetworkImageProvider('https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQyhahkcNF9i-UtMAOBryg2Ao6f2NJ2JIHtv64I9VC4mjagj7mr'),
+                        image: CachedNetworkImageProvider(
+                            'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQyhahkcNF9i-UtMAOBryg2Ao6f2NJ2JIHtv64I9VC4mjagj7mr'),
                         fit: BoxFit.cover,
                       ),
                     ),
@@ -91,7 +97,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Customfont(
-                      text:  widget.username,
+                      text: widget.username,
                       fontWeight: FontWeight.bold,
                       fontSize: ScreenUtil().setSp(20),
                       color: Colors.black,
@@ -180,97 +186,107 @@ class _ProfileScreenState extends State<ProfileScreen> {
                   children: [
                     Container(
                       color: Colors.white,
-                      child: SingleChildScrollView(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            const SizedBox(height: 20),
-                            Postcard(
-                              userName: 'Vhon Liquigan',
-                              postContent: 'First time playing AFK Journey',
-                              profileImageUrl: 'https://gdm-assets.b-cdn.net/images/ncavvykf/epicstream/c1e0142f06accc89f5ccdba411b09401a95f0cde-1280x720.jpg?rect=0,26,1280,669&w=398&h=208&auto=format',
-                              imageUrl: 'https://s2.mmommorpg.com/media/sshots/scaled/afk-journey-preview2.jpg.1156x640_q70_crop-smart.jpg',
-                              dateText: '12m',
-                              commentCount: 2,
-                              shareCount: 5,
-                              initialLikeCount: 20,
-                              onLikePressed: () {},
-                              onCommentPressed: () {},
-                              onSharePressed: () {},
-                              commentController: TextEditingController(),
-                              onCommentChanged: (text) {},
-                              commentsList: const [],
-                            ),
-                            const SizedBox(height: 20),
-                            Postcard(
-                              userName: 'Vhon Liquigan',
-                              postContent: 'I like yaa!',
-                              profileImageUrl: 'https://gdm-assets.b-cdn.net/images/ncavvykf/epicstream/c1e0142f06accc89f5ccdba411b09401a95f0cde-1280x720.jpg?rect=0,26,1280,669&w=398&h=208&auto=format',
-                              imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBvkXMVIMKaf3zr2Og37J5xymgYQYc9fbIyZylIc7QTBiDVuMU',
-                              dateText: '1h',
-                              commentCount: 2,
-                              shareCount: 3,
-                              initialLikeCount: 6,
-                              onLikePressed: () {},
-                              onCommentPressed: () {},
-                              onSharePressed: () {},
-                              commentController: TextEditingController(),
-                              onCommentChanged: (text) {},
-                              commentsList: const [],
-                            ),
-                            const SizedBox(height: 20),
-                            Postcard(
-                              userName: 'Vhon Liquigan',
-                              postContent: 'I miss them already :(',
-                              profileImageUrl: 'https://gdm-assets.b-cdn.net/images/ncavvykf/epicstream/c1e0142f06accc89f5ccdba411b09401a95f0cde-1280x720.jpg?rect=0,26,1280,669&w=398&h=208&auto=format',
-                              imageUrl: 'https://media.pitchfork.com/photos/635bd5c994075a586409fc23/master/w_1280%2Cc_limit/Brockhampton-The-Family.jpg',
-                              dateText: 'December 22 at 12:27 AM',
-                              commentCount: 2,
-                              shareCount: 1,
-                              initialLikeCount: 11,
-                              onLikePressed: () {},
-                              onCommentPressed: () {},
-                              onSharePressed: () {},
-                              commentController: TextEditingController(),
-                              onCommentChanged: (text) {},
-                              commentsList: const [],
-                            ),
-                            const SizedBox(height: 20),
-                            Postcard(
-                              userName: 'Vhon Liquigan',
-                              postContent: 'ThE bEsT y3ar5 of OUR L1v3s',
-                              profileImageUrl: 'https://gdm-assets.b-cdn.net/images/ncavvykf/epicstream/c1e0142f06accc89f5ccdba411b09401a95f0cde-1280x720.jpg?rect=0,26,1280,669&w=398&h=208&auto=format',
-                              imageUrl: '', // No image
-                              dateText: 'December 7 at 3:26 PM',
-                              commentCount: 0,
-                              shareCount: 0,
-                              initialLikeCount: 8,
-                              onLikePressed: () {},
-                              onCommentPressed: () {},
-                              onSharePressed: () {},
-                              commentController: TextEditingController(),
-                              onCommentChanged: (text) {},
-                              commentsList: const [],
-                            ),
-                            const SizedBox(height: 20),
-                            Postcard(
-                              userName: 'Vhon Liquigan',
-                              postContent:'Keep a gold chain on my neck, fly as a jet :o',
-                              profileImageUrl: 'https://gdm-assets.b-cdn.net/images/ncavvykf/epicstream/c1e0142f06accc89f5ccdba411b09401a95f0cde-1280x720.jpg?rect=0,26,1280,669&w=398&h=208&auto=format',
-                              imageUrl: '', // No image
-                              dateText: 'December 6 at 10:08 AM',
-                              commentCount: 2,
-                              shareCount: 3,
-                              initialLikeCount: 13,
-                              onLikePressed: () {},
-                              onCommentPressed: () {},
-                              onSharePressed: () {},
-                              commentController: TextEditingController(),
-                              onCommentChanged: (text) {},
-                              commentsList: const [],
-                            ),
-                          ],
-                        ),
+                      child: ListView(
+                        shrinkWrap: true,
+                        physics: const NeverScrollableScrollPhysics(),
+                        children: [
+                          Column(
+                            children: [
+                              const SizedBox(height: 20),
+                              Postcard(
+                                userName: 'Vhon Liquigan',
+                                postContent: 'First time playing AFK Journey',
+                                profileImageUrl:
+                                    'https://gdm-assets.b-cdn.net/images/ncavvykf/epicstream/c1e0142f06accc89f5ccdba411b09401a95f0cde-1280x720.jpg?rect=0,26,1280,669&w=398&h=208&auto=format',
+                                imageUrl:
+                                    'https://s2.mmommorpg.com/media/sshots/scaled/afk-journey-preview2.jpg.1156x640_q70_crop-smart.jpg',
+                                dateText: '12m',
+                                commentCount: 2,
+                                shareCount: 5,
+                                initialLikeCount: 20,
+                                onLikePressed: () {},
+                                onCommentPressed: () {},
+                                onSharePressed: () {},
+                                commentController: TextEditingController(),
+                                onCommentChanged: (text) {},
+                                commentsList: const [],
+                              ),
+                              const SizedBox(height: 20),
+                              Postcard(
+                                userName: 'Vhon Liquigan',
+                                postContent: 'I like yaa!',
+                                profileImageUrl:
+                                    'https://gdm-assets.b-cdn.net/images/ncavvykf/epicstream/c1e0142f06accc89f5ccdba411b09401a95f0cde-1280x720.jpg?rect=0,26,1280,669&w=398&h=208&auto=format',
+                                imageUrl:
+                                    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBvkXMVIMKaf3zr2Og37J5xymgYQYc9fbIyZylIc7QTBiDVuMU',
+                                dateText: '1h',
+                                commentCount: 2,
+                                shareCount: 3,
+                                initialLikeCount: 6,
+                                onLikePressed: () {},
+                                onCommentPressed: () {},
+                                onSharePressed: () {},
+                                commentController: TextEditingController(),
+                                onCommentChanged: (text) {},
+                                commentsList: const [],
+                              ),
+                              const SizedBox(height: 20),
+                              Postcard(
+                                userName: 'Vhon Liquigan',
+                                postContent: 'I miss them already :(',
+                                profileImageUrl:
+                                    'https://gdm-assets.b-cdn.net/images/ncavvykf/epicstream/c1e0142f06accc89f5ccdba411b09401a95f0cde-1280x720.jpg?rect=0,26,1280,669&w=398&h=208&auto=format',
+                                imageUrl:
+                                    'https://media.pitchfork.com/photos/635bd5c994075a586409fc23/master/w_1280%2Cc_limit/Brockhampton-The-Family.jpg',
+                                dateText: 'December 22 at 12:27 AM',
+                                commentCount: 2,
+                                shareCount: 1,
+                                initialLikeCount: 11,
+                                onLikePressed: () {},
+                                onCommentPressed: () {},
+                                onSharePressed: () {},
+                                commentController: TextEditingController(),
+                                onCommentChanged: (text) {},
+                                commentsList: const [],
+                              ),
+                              const SizedBox(height: 20),
+                              Postcard(
+                                userName: 'Vhon Liquigan',
+                                postContent: 'ThE bEsT y3ar5 of OUR L1v3s',
+                                profileImageUrl:
+                                    'https://gdm-assets.b-cdn.net/images/ncavvykf/epicstream/c1e0142f06accc89f5ccdba411b09401a95f0cde-1280x720.jpg?rect=0,26,1280,669&w=398&h=208&auto=format',
+                                dateText: 'December 7 at 3:26 PM',
+                                commentCount: 0,
+                                shareCount: 0,
+                                initialLikeCount: 8,
+                                onLikePressed: () {},
+                                onCommentPressed: () {},
+                                onSharePressed: () {},
+                                commentController: TextEditingController(),
+                                onCommentChanged: (text) {},
+                                commentsList: const [],
+                              ),
+                              const SizedBox(height: 20),
+                              Postcard(
+                                userName: 'Vhon Liquigan',
+                                postContent:
+                                    'Keep a gold chain on my neck, fly as a jet :o',
+                                profileImageUrl:
+                                    'https://gdm-assets.b-cdn.net/images/ncavvykf/epicstream/c1e0142f06accc89f5ccdba411b09401a95f0cde-1280x720.jpg?rect=0,26,1280,669&w=398&h=208&auto=format',
+                                dateText: 'December 6 at 10:08 AM',
+                                commentCount: 2,
+                                shareCount: 3,
+                                initialLikeCount: 13,
+                                onLikePressed: () {},
+                                onCommentPressed: () {},
+                                onSharePressed: () {},
+                                commentController: TextEditingController(),
+                                onCommentChanged: (text) {},
+                                commentsList: const [],
+                              ),
+                            ],
+                          ),
+                        ],
                       ),
                     ),
                     const profile_about(),
